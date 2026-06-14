6972c070eeda33e96f906bc60eaaafa117012560
DHranjec <daliborka.hranjec@gmail.com>
2026-06-14 13:46:55 +0200

    fix: Improve plotting helpers
    Generalize plot_curves to support both loss and accuracy metrics.
    Fix data type handling in plot_decision_boundary.

--- src/plotting_helpers.py
+++ src/plotting_helpers.py
@@ -33,42 +33,50 @@ def plot_predictions(train_data, train_labels,
   plt.savefig(filename)
   plt.show()
 
-def plot_loss_curves(epoch_count, train_loss_values, test_loss_values, filename="loss_curves.png"):
+def plot_curves(epoch_values, train_values, test_values, var_to_plot, filename=f"curves.png"):
   """
   Plots training and test loss curves.
-  :param epoch_count:
-  :param train_loss_values:
-  :param test_loss_values:
-  :param filename:
+  :param epoch_values: list of number of epochs
+  :param train_values: list of training values
+  :param test_values: list of testing values
+  :param var_to_plot: "loss" or "accuracy"
+  :param filename: name of the file to save the curves
   :return:
   """
   plt.figure(figsize=(8, 6))
 
   # Plot train loss curve
-  plt.plot(epoch_count, torch.tensor(train_loss_values).cpu().numpy(), label="Train loss")
+  plt.plot(epoch_values, torch.tensor(train_values).cpu().numpy(), label=f"Train {var_to_plot}")
 
   # Plot test loss curve
-  plt.plot(epoch_count, torch.tensor(test_loss_values).cpu().numpy(), label="Test loss")
+  plt.plot(epoch_values, torch.tensor(test_values).cpu().numpy(), label=f"Test {var_to_plot}")
 
-  plt.title("Loss curves")
+  plt.title(f"{var_to_plot.capitalize()} curves")
   plt.xlabel("Epoch")
-  plt.ylabel("Loss")
+  plt.ylabel(var_to_plot.capitalize())
   plt.legend()
 
   plt.savefig(filename)
   plt.show()
 
-def plot_decision_boundary(model, X, y, cmap=plt.cm.RdYlBu):
+def plot_decision_boundary(model, X, y, cmap="cool"):
+  """
+  Plots the decision boundary fitted by model for binary or multiclass classification.
+  :param model: fitted model
+  :param X: data
+  :param y: labels
+  :return:
+  """
   # Put everything on CPU
   model.to("cpu")
   X = X.to("cpu")
   y = y.to("cpu")
 
-  x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-  y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-  x_mesh, y_mesh = np.meshgrid(np.linspace(x_min, x_max, 100),
-                               np.linspace(y_min, y_max, 100))
-  X_from_grid = torch.from_numpy(np.column_stack((x_mesh.ravel(), y_mesh.ravel())))
+  x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+  y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+  x_mesh, y_mesh = np.meshgrid(np.linspace(x_min, x_max, 500),
+                               np.linspace(y_min, y_max, 500))
+  X_from_grid = torch.from_numpy(np.column_stack((x_mesh.ravel(), y_mesh.ravel()))).type(torch.float)
 
   model.eval()
   with torch.inference_mode():
@@ -81,7 +89,7 @@ def plot_decision_boundary(model, X, y, cmap=plt.cm.RdYlBu):
 
   y_pred = y_pred.reshape(x_mesh.shape).detach().numpy()
 
-  plt.contourf(x_mesh, y_mesh, y_pred, cmap=plt.cm.RdYlBu, alpha=0.8)
-  plt.scatter(X[:, 0], X[:, 1], c=y, s=4, cmap=plt.cm.RdYlBu)
+  plt.contourf(x_mesh, y_mesh, y_pred, cmap=cmap, alpha=0.2)
+  plt.scatter(X[:, 0], X[:, 1], c=y, s=4, cmap=cmap)
   plt.xlim(x_min, x_max)
   plt.ylim(y_min, y_max)
