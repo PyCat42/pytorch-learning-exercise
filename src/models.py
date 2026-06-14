54b24b9935b469642513c0d86bbac16ce1127520
DHranjec <daliborka.hranjec@gmail.com>
2026-06-14 13:44:19 +0200

    feat: Add CircleBinClassModel to models.py
    Add CircleBinClassModel for binary classification of circles with different labels.

--- src/models.py
+++ src/models.py
@@ -14,3 +14,19 @@ class SimpleLinearRegressionModel(nn.Module):
     """
     return self.linear_layer(x)
 
+class CircleBinClassModel(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.layer_1 = nn.Linear(in_features=2, out_features=30)
+    self.layer_2 = nn.Linear(in_features=30, out_features=15)
+    self.layer_3 = nn.Linear(in_features=15, out_features=1)
+    self.relu = nn.ReLU()
+
+  def forward(self, x):
+    """
+    Forward pass of the linear regression model.
+    :param x: torch.Tensor
+    :return: torch.Tensor
+    """
+    # Apply ReLU after each hidden layer
+    return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
