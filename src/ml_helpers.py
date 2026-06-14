db6e6e137f7458d4a56b170913e126f7e6c3d27c
DHranjec <daliborka.hranjec@gmail.com>
2026-06-14 13:49:45 +0200

    docs: Docstring update in ml_helpers.py

--- src/ml_helpers.py
+++ src/ml_helpers.py
@@ -3,9 +3,12 @@ import torch
 def train_test_split(X_data, y_data, train_size=0.8):
     """
     Split data into train and test sets.
+    Built-in function:
+    sklearn.model_selection.train_test_split(X, y, test_size=None, train_size=None,
+     random_state=None, shuffle=True, stratify=None)
     :param X_data: data
     :param y_data: labels
-    :param train_size:
+    :param train_size: size of training set (from 0 to 1)
     :return: X_train, y_train, X_test, y_test
     """
     num_samples = len(X_data)
